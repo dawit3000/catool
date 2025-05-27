838b6c253c03669cdcf0b0ebc4a0dfaa0e50b538
Dawit Aberra <dawit3000@hotmail.com>
2025-05-27 19:21:59 -0400

    2

--- R/schedule.R
+++ R/schedule.R
@@ -14,5 +14,24 @@
 #'   \item{INSTRUCTOR}{Name of Instructor Teaching the course—real names changed; Char "Bui, Analie" "Calhoun, Alexandra" "Bui, Analie" "Davis, Kheylen" ...}
 #' }
 #'
-#' @source [Insert any sources if applicable]
+#' @source Included in package as data/schedule.csv
+"schedule"
+
+#' Schedule Data
+#'
+#' A dataset containing the schedule information.
+#'
+#' @description
+#' This dataset represents a sample class schedule used by several institutions, with some variations in variable names.
+#' Users are encouraged to ensure that the key variables—`HRS`, `ENRLD`, and `INSTRUCTOR`—are formatted correctly for the package to function properly.
+#'
+#' @details
+#' The dataset has 665 rows and 15 columns, including:
+#' \describe{
+#'   \item{HRS}{Credit Hours of a course: int  3 3 3 3 3 3 3 0 3 0 ...}
+#'   \item{ENRLD}{Number of students enrolled in a class: int  34 32 34 7 9 8 31 31 12 12 ...}
+#'   \item{INSTRUCTOR}{Name of Instructor Teaching the course—real names changed; Char "Bui, Analie" "Calhoun, Alexandra" "Bui, Analie" "Davis, Kheylen" ...}
+#' }
+#'
+#' @source Included in package as data/schedule.csv
 "schedule"
